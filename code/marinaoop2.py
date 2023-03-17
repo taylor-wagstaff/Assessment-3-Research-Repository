0b079b83a11d10bd5f27993a0a5e644154647aa8
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-03-17 21:43:21 +1300

    changes

--- code/marinaoop2.py
+++ code/marinaoop2.py
@@ -67,6 +67,13 @@ cat1 = DomesticCat("Jane", "Peter", 5, True)
 # cat1.age = 5
 # cat1.isHappy = True
 
+
+########################################
+## main process, 
+
+# from Cat import *
+
+# if __name__ == "__main__"
 # call the method
 cat1.display()
 cat1.sound()
@@ -80,3 +87,45 @@ cat2.sound()
 cat2.hunt()
 
 #polymorphism, somehting can have more than one form, 
+
+########################################
+##OOPS concept, 
+
+# Encapualtion, Inheritance, Polymorphism, Abstraction, 
+
+# Encapualtion: to protect, something, class is the best example, 
+# if you need to access something, use the object. 
+
+# access specifier, public, public and protected. decide flexibilty to your user. 
+# private: authroized only, 
+# Protected: everywhere can see, only authrozied can use, like read/write only. 
+
+# Inheritance: A:  base, parent, super class, B: derive, child, sub class. three differnt termologies, 
+# Single level or simple inheritance. 
+# if more inheritance, multi-level if more sub classes. 
+# if two base classes, multiple inheritance. 
+
+# hierarchical, two sub classes inherit from one base class. 
+# Hybird, combing two subclass together in a single figure. 
+# Python is the only language that supports all types of inheritance, 
+
+# Abstraction
+
+# If something is not required, hide, hide complexity from the user
+
+# Polymorphism 
+
+# Proceedural(functional programming) orientated, 
+# Object Orientated Programming, method instead of funciton. 
+########################################
+
+########################################
+### Marina Lesson, 17th of March, Software Development life cycle. 
+
+# planning: which problem are we going to solve, time, price etc, 
+# Analysis: Decision is made, mid-level manager, collect requirments, talk to stakeholders. Must be formalised. 
+# Design: Which software and hardware architecture, Data model, Wireframes
+# Implementation: Already have your structure designed, coding, actually making. 
+# Testing & intergration: are your requirements all implementated
+# Maintenance: decide, how are you going to improve it. 
+########################################
