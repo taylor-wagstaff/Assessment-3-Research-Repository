f94fb9ca1c53c8b85454dcff7971b07399820b54
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-03-31 10:25:43 +1300

    added web scraper

--- code/lessons/scraper.py
+++ code/lessons/scraper.py
@@ -1,22 +1,120 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 import requests
+import random
 from bs4 import BeautifulSoup
 
-### A web scraper is an api, 
-### an Api for a website that doesnt have an api. 
 
-url = "https://pixelford.com/blog/"
+url = "https://www.economist.com/"
 
-
-response = requests.get(url)
-
-html = response.content
-# beautiful soup is parser for data, give varibles
-soup = BeautifulSoup(html, "html.parser")
-a_tags = soup.find_all('a', class_='entry-title-link')
+def generate_poem_words():
+  # url= input("What is the url?  ")
+  response = requests.get(url)
 
 
-for a_tag in a_tags:
-    print(a_tag.get_text())
+  # encoder 
+  response.encoding = 'utf-8'  # set the encoding to utf-8
+  encoding = response.encoding
 
-titles = list(map(lambda a_tag: a_tag.get_text(), a_tags))
-print(titles)
+
+  html = response.content.decode(encoding)
+  # beautiful soup is parser for data, give varibles
+  soup = BeautifulSoup(html, "html.parser")
+  h3_tags = soup.find_all('h3')
+  images = soup.find_all('img')
+
+  # varible to store url
+  imageSources = []
+
+  for image in images:
+      imageSources.append(image.get("src"))
+
+
+  # print(imageSources)
+
+
+
+
+  # varible to store all headlines
+  headlines = []
+
+  # iterate over all h3 tags
+  for h3_tag in h3_tags:
+      h = h3_tag.get_text()
+      #store in varible
+      headlines.append(h)
+
+
+
+
+
+
+  all_headlines = ' '.join(map(str, headlines))
+  split_words = all_headlines.split()
+  words_to_remove = ['The', 'the', 'and', 'And', 'A','a']
+
+  for x in words_to_remove:
+      while x in split_words:
+          split_words.remove(x)
+
+
+
+  random_headline = random.sample(split_words,  min(9, len(split_words)))
+  # new_headlines = ' '.join(map(str, random_headline))
+
+
+  poem_words = []
+  # print random words from all headlines from website 
+  for words in random_headline:
+      w = words
+      poem_words.append(w)
+
+  print(poem_words)
+
+  new_words = [word.replace('’', "'") for word in poem_words]
+
+  poem_str = "<br>".join(new_words)
+
+  return poem_str
+
+
+
+
+
+###### server ######
+
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Economist Headline Poem Generator</title></head>", "utf-8"))
+        self.wfile.write(bytes('<meta http-equiv="refresh" content="1">', "utf-8"))
+        self.wfile.write(bytes("<style>", "utf-8"))
+        self.wfile.write(bytes("body { font-family:	Times New Roman, serif; font-size: 30px; background-color: red; color: white; }", "utf-8"))
+        self.wfile.write(bytes("</style></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Economist Poem Generator: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes(f"<h1>{poem_words}</h1>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        while True:
+          poem_words = generate_poem_words()
+          # webServer.serve_forever()
+          webServer.handle_request()
+          time.sleep(10)
+         
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
--- code/lessons/scraper_venv/bin/activate.fish
+++ code/lessons/scraper_venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/taylorwagstaff/Personal Projects/whitecliffe/assignments/Assessment-3-Research-Repository/code/lessons/scraper_venv"
+set -gx VIRTUAL_ENV "/Users/taylorwagstaff/projects/whitecliffe/assignments/Assessment-3-Research-Repository/code/lessons/scraper_venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
